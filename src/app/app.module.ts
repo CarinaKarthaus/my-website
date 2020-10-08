5363ad5ec5296e691d8379065314f1f7a5eb31b3
CarinaKarthaus <carina.karthaus@gmail.com>
2020-10-08 21:06:25 +0200

    Skill-bars for about-section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 
 
@@ -15,12 +17,14 @@ import { AboutComponent } from './about/about.component';
     AppComponent,
     HomeComponent,
     MenuComponent,
-    AboutComponent
+    AboutComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
