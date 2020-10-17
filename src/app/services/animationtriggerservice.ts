3cd52b8b90bb883647bcd59297fde50d23e4278c
CarinaKarthaus <carina.karthaus@gmail.com>
2020-10-17 20:42:35 +0200

    Refactoring of animation trigger service

--- src/app/services/animationtriggerservice.ts
+++ src/app/services/animationtriggerservice.ts
@@ -1,78 +1,41 @@
 import { HostListener, Injectable } from '@angular/core';
+import { animationState, elementOffsetTop, animTriggerPosition } from '../../assets/data/animationtriggers';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimationTriggerService {
-
-  constructor() { }
-
-  public animationState = {
-    'about_header': 'out',
-    'about_labels': 'out',
-    'about_skills': 'out',
-    'portfolio_header': 'out',
-    'portfolio_filter': 'out',
-    'portfolio': 'out',
-    'contact_header': 'out',
-    'contact': 'out',
-    'contact_dialog': 'out',
-    'footer': 'out'
-  };
-
   currentPagePosition: number;
   triggerOffset: number;
   windowHeight: number;
 
-  // Indicates offset of HTML-element to the document's top edge
-  public elementOffsetTop = {
-    home: 1000,
-    about_header: 1050,
-    about_labels: 1100,
-    about_skills: 1200,
-    portfolio_header: 1800,
-    portfolio_filter: 2000,
-    portfolio: 2100,
-    contact_header: 2700,
-    contact_dialog: 2800,
-    footer: 4000,
-  };
+  public animationState = animationState;
+  public elementOffsetTop = elementOffsetTop;     // Indicates offset of HTML-element to the document's top edge
+  public animTriggerPosition = animTriggerPosition;
+  triggerPos = this.animTriggerPosition;
 
-  public animTriggerPosition = {
-    home: 1000,
-    about_header: 1050,
-    about_labels: 1100,
-    about_skills: 1200,
-    portfolio_header: 1800,
-    portfolio_filter: 2000,
-    portfolio: 2100,
-    contact_header: 2700,
-    contact_dialog: 2800,
-    footer: 3000,
-  }
+  constructor() { }
 
 
   calculateTriggerPositions() {
-    this.windowHeight = window.innerHeight;
-    this.triggerOffset =  window.innerHeight / 2  ;
+    let offsetTop = this.elementOffsetTop;
+    let triggerOffset = window.innerHeight / 1.6; // adjust to trigger before element reaches top of screen 
 
-    this.animTriggerPosition.home = this.windowHeight;
-    this.animTriggerPosition.about_header = this.elementOffsetTop.about_header - this.windowHeight + this.triggerOffset;
-    this.animTriggerPosition.about_labels = this.elementOffsetTop.about_labels - this.windowHeight + this.triggerOffset;
-    this.animTriggerPosition.about_skills = this.elementOffsetTop.about_skills - this.windowHeight + this.triggerOffset;
+    this.triggerPos.home = this.windowHeight;
+    this.triggerPos.about_header = offsetTop.about_header - triggerOffset;
+    this.triggerPos.about_labels = offsetTop.about_labels - triggerOffset;
+    this.triggerPos.about_skills = offsetTop.about_skills - triggerOffset;
 
-    this.animTriggerPosition.portfolio_header = this.elementOffsetTop.portfolio_header - this.windowHeight + this.triggerOffset;
-    this.animTriggerPosition.portfolio_filter = this.elementOffsetTop.portfolio_filter - this.windowHeight + this.triggerOffset;
-    this.animTriggerPosition.portfolio = this.elementOffsetTop.portfolio - this.windowHeight + this.triggerOffset;    
+    this.triggerPos.portfolio_header = offsetTop.portfolio_header - triggerOffset;
+    this.triggerPos.portfolio_filter = offsetTop.portfolio_filter - triggerOffset;
+    this.triggerPos.portfolio = offsetTop.portfolio - triggerOffset;    
     
-    this.animTriggerPosition.contact_header = this.elementOffsetTop.contact_header - this.windowHeight + this.triggerOffset;
-    this.animTriggerPosition.contact_dialog = this.elementOffsetTop.contact_dialog - this.windowHeight + this.triggerOffset;
-    this.animTriggerPosition.footer = this.elementOffsetTop.footer - this.windowHeight + this.triggerOffset;
-
+    this.triggerPos.contact_header = offsetTop.contact_header - triggerOffset;
+    this.triggerPos.contact_dialog = offsetTop.contact_dialog - triggerOffset;
+    this.triggerPos.footer = offsetTop.footer - triggerOffset;
 
     console.log('elementOffsetService:', this.elementOffsetTop);
-
   }
 
   /**
@@ -83,47 +46,47 @@ export class AnimationTriggerService {
     this.currentPagePosition = window.scrollY;
     this.calculateTriggerPositions();
 
-    let triggerPos = this.animTriggerPosition;
-
-    // this.triggerAboutSection();
-
-    
-    if (this.currentPagePosition > triggerPos.about_header) {  
-      this.animationState.about_header = 'in'
-    } 
-    if (this.currentPagePosition > triggerPos.about_labels) { 
-      this.animationState.about_labels = 'in' 
-    }
-    if (this.currentPagePosition > triggerPos.about_skills) { 
-      this.animationState.about_skills = 'in' 
-    }
-    if (this.currentPagePosition > triggerPos.portfolio_header) { 
-      this.animationState.portfolio_header = 'in' 
-    }
-    if (this.currentPagePosition > triggerPos.portfolio_filter) { 
-      this.animationState.portfolio_filter = 'in' 
-    }
-    if (this.currentPagePosition > triggerPos.portfolio) { 
-      this.animationState.portfolio = 'in' 
-    }
-    if (this.currentPagePosition > triggerPos.contact_header) { 
-      this.animationState.contact_header = 'in' 
-    }
-    if (this.currentPagePosition > triggerPos.contact_dialog) { 
-      this.animationState.contact = 'in' 
-    }  
-    if (this.currentPagePosition > triggerPos.footer) { 
-      this.animationState.footer = 'in' 
-    }
+    this.triggerAboutSection();
+    this.triggerPortfolioSection();
+    this.triggerContactSection();
     
     // console.log(this.currentPagePosition);
-    console.log('window.scrollY', this.currentPagePosition);
-    
-    
     return this.animationState;  
   }
 
-  // triggerAboutSection() {
+  triggerAboutSection() {
+    if (this.currentPagePosition > this.triggerPos.about_header) {  
+      this.animationState.about_header = 'in'
+    } 
+    if (this.currentPagePosition > this.triggerPos.about_labels) { 
+      this.animationState.about_labels = 'in' 
+    }
+    if (this.currentPagePosition > this.triggerPos.about_skills) { 
+      this.animationState.about_skills = 'in' 
+    }
+  }
 
-  // }
+  triggerPortfolioSection() {
+    if (this.currentPagePosition > this.triggerPos.portfolio_header) { 
+      this.animationState.portfolio_header = 'in' 
+    }
+    if (this.currentPagePosition > this.triggerPos.portfolio_filter) { 
+      this.animationState.portfolio_filter = 'in' 
+    }
+    if (this.currentPagePosition > this.triggerPos.portfolio) { 
+      this.animationState.portfolio = 'in' 
+    }
+  }
+
+  triggerContactSection() {
+    if (this.currentPagePosition > this.triggerPos.contact_header) { 
+      this.animationState.contact_header = 'in' 
+    }
+    if (this.currentPagePosition > this.triggerPos.contact_dialog) { 
+      this.animationState.contact = 'in' 
+    }  
+    if (this.currentPagePosition > this.triggerPos.footer) { 
+      this.animationState.footer = 'in' 
+    }
+  }
 }
---
+++ src/assets/data/animationtriggers.ts
@@ -0,0 +1,43 @@
+// Default states for animation states, offsets & triggers
+
+var animationState = {
+    'about_header': 'out',
+    'about_labels': 'out',
+    'about_skills': 'out',
+    'portfolio_header': 'out',
+    'portfolio_filter': 'out',
+    'portfolio': 'out',
+    'contact_header': 'out',
+    'contact': 'out',
+    'contact_dialog': 'out',
+    'footer': 'out'
+  };
+
+  var elementOffsetTop = {
+    home: 1000,
+    about_header: 1050,
+    about_labels: 1100,
+    about_skills: 1200,
+    portfolio_header: 1800,
+    portfolio_filter: 2000,
+    portfolio: 2100,
+    contact_header: 2700,
+    contact_dialog: 2800,
+    footer: 4000,
+  };
+
+  var animTriggerPosition = {
+    home: 1000,
+    about_header: 1050,
+    about_labels: 1100,
+    about_skills: 1200,
+    portfolio_header: 1800,
+    portfolio_filter: 2000,
+    portfolio: 2100,
+    contact_header: 2700,
+    contact_dialog: 2800,
+    footer: 3000,
+  }
+
+  export {animationState, elementOffsetTop, animTriggerPosition};
+
